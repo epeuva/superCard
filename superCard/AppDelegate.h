79bf917de4a8f28ddfeb8249fdd6fc43f5cfa044
epeuva <epeuva@gmail.com>
2018-07-23 18:46:43 +0200

    Init project

---
+++ superCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  superCard
+//
+//  Created by epeuva on 23/07/2018
+//  Copyright © 2018 epeuva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ superCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  superCard
+//
+//  Created by epeuva on 23/07/2018
+//  Copyright © 2018 epeuva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
