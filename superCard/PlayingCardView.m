3a9be9da7996593fd7e80a69bfb2d460900d1d5f
epeuva <epeuva@gmail.com>
2018-07-24 16:16:37 +0200

    Finished lecture 7. Smaller card icons

--- superCard/PlayingCardView.m
+++ superCard/PlayingCardView.m
@@ -61,10 +61,86 @@
     return @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"][self.rank];
 }
 
+#pragma mark - Pips
+
+#define PIP_HOFFSET_PERCENTAGE 0.165
+#define PIP_VOFFSET1_PERCENTAGE 0.090
+#define PIP_VOFFSET2_PERCENTAGE 0.175
+#define PIP_VOFFSET3_PERCENTAGE 0.270
+
+- (void)drawPips
+{
+    if ((self.rank == 1) || (self.rank == 5) || (self.rank == 9) || (self.rank == 3)) {
+        [self drawPipsWithHorizontalOffset:0
+                            verticalOffset:0
+                        mirroredVertically:NO];
+    }
+    if ((self.rank == 6) || (self.rank == 7) || (self.rank == 8)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:0
+                        mirroredVertically:NO];
+    }
+    if ((self.rank == 2) || (self.rank == 3) || (self.rank == 7) || (self.rank == 8) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:0
+                            verticalOffset:PIP_VOFFSET2_PERCENTAGE
+                        mirroredVertically:(self.rank != 7)];
+    }
+    if ((self.rank == 4) || (self.rank == 5) || (self.rank == 6) || (self.rank == 7) || (self.rank == 8) || (self.rank == 9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:PIP_VOFFSET3_PERCENTAGE
+                        mirroredVertically:YES];
+    }
+    // draws 4 pips in middle
+    if ((self.rank == 9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:PIP_VOFFSET1_PERCENTAGE
+                        mirroredVertically:YES];
+    }
+}
+
+#define PIP_FONT_SCALE_FACTOR 0.012
+
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                          upsideDown:(BOOL)upsideDown
+{
+    if (upsideDown) [self pushContextAndRotateUpsideDown];
+    CGPoint middle = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+    UIFont *pipFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    pipFont = [pipFont fontWithSize:[pipFont pointSize] * self.bounds.size.width * PIP_FONT_SCALE_FACTOR];
+    NSAttributedString *attributedSuit = [[NSAttributedString alloc] initWithString:self.suit attributes:@{ NSFontAttributeName : pipFont }];
+    CGSize pipSize = [attributedSuit size];
+    CGPoint pipOrigin = CGPointMake(
+                                    middle.x-pipSize.width/2.0-hoffset*self.bounds.size.width,
+                                    middle.y-pipSize.height/2.0-voffset*self.bounds.size.height
+                                    );
+    [attributedSuit drawAtPoint:pipOrigin];
+    if (hoffset) {
+        pipOrigin.x += hoffset*2.0*self.bounds.size.width;
+        [attributedSuit drawAtPoint:pipOrigin];
+    }
+    if (upsideDown) [self popContext];
+}
+
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                  mirroredVertically:(BOOL)mirroredVertically
+{
+    [self drawPipsWithHorizontalOffset:hoffset
+                        verticalOffset:voffset
+                            upsideDown:NO];
+    if (mirroredVertically) {
+        [self drawPipsWithHorizontalOffset:hoffset
+                            verticalOffset:voffset
+                                upsideDown:YES];
+    }
+}
+
 #pragma mark - Drawing
 
 #define CORNER_FONT_STANDHRD_HEIGHT 100.0
 #define CORNER_RADIUS 12.0
+#define CUSTOM_TEXT_MULTIPLIER 0.4
 
 - (CGFloat)cornerScaleFactor { return self.bounds.size.height/CORNER_FONT_STANDHRD_HEIGHT; }
 - (CGFloat)cornerRadius { return CORNER_RADIUS * [self cornerScaleFactor]; }
@@ -106,8 +182,17 @@
     
 }
 
-- (void)drawPips {
-    
+- (void)pushContextAndRotateUpsideDown
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    CGContextTranslateCTM(context, self.bounds.size.width, self.bounds.size.height);
+    CGContextRotateCTM(context, M_PI);
+}
+
+- (void)popContext
+{
+    CGContextRestoreGState(UIGraphicsGetCurrentContext());
 }
 
 - (void)drawCorners
@@ -116,7 +201,7 @@
     paragraphStyle.alignment = NSTextAlignmentCenter;
     
     UIFont *cornerFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-    cornerFont = [cornerFont fontWithSize:cornerFont.pointSize * [self cornerScaleFactor]];
+    cornerFont = [cornerFont fontWithSize:cornerFont.pointSize * [self cornerScaleFactor] / CUSTOM_TEXT_MULTIPLIER];
     
     NSAttributedString *cornerText = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@\n%@", [self rankAsString], self.suit] attributes:@{ NSFontAttributeName : cornerFont, NSParagraphStyleAttributeName : paragraphStyle }];
     
